640c5f6489b398aeeb9a2a8168ef2876f334dd78
fanfandesbois <fanny.bachey@gmail.com>
2022-11-22 00:10:33 +0100

    JDB + script

--- Exercices/traitement_url_base.sh
+++ Exercices/traitement_url_base.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 #===============================================================================
-Comportement du programme:
+#Comportement du programme:
 # Script capable de générer les premiers tableaux de données concernant mes URL.
 # Comment on lance le programme : 
 # Paramètres: 
 #===============================================================================
 
 fichier_urls=$1 # le fichier d'URL en entrée
-fichier_tableau=$2 # le fichier HTML en sortie
+#fichier_tableau=$2 # le fichier HTML en sortie
 
 # !!!!!!
 # ici on doit vérifier que nos deux paramètres existent, sinon on ferme!
@@ -17,15 +17,18 @@ fichier_tableau=$2 # le fichier HTML en sortie
 # -IL => réponse du serveur + suit les intéractions
 # modifier la ligne suivante pour créer effectivement du HTML
 
-echo "Je dois devenir du code HTML à partir de la question 3" > $fichier_tableau
+#echo "Je dois devenir du code HTML à partir de la question 3" > $fichier_tableau
 
 lineno=1;
 
 while read -r line;
 do
+	n_lines=$(curl -ILs $line);
+	#dans une première variable je range l'entête 
+	n_lines2=$(curl -ILs $line | head -n 1 );
+	#dans une deuxième, je mets la première ligne de la variable. 
+	echo $n_lines2;
 	echo "ligne $lineno: $line";
 	lineno=$((lineno+1));
-	n_lines=$(curl -IL $line | head -n 1  )
-	#le code de réponse se trouve sur la première ligne 
-	head -n ligne_x nom_du_fichier
 done < $fichier_urls
+
--- Journal-de-bord.md
+++ Journal-de-bord.md
@@ -17,4 +17,14 @@ Avancement sur le ppe :
 - Recherches d'URL en portugais 
 - Début de l'explication du choix de mot et de notre démarche. 
 
-## 
+## 8 novembre 2022
+Création du site web relié à mon dépôt git. 
+- Premières manipulations d'HTML. 
+- Premier script bash pour les URL.
+
+## 15 novembre 2022
+On avance sur le script en bash pour les URL. 
+Création du tableau pour les URL. 
+
+## 22 novembre 2022 
+J'arrive à faire fonctionner mon script mais je n'arrive pas 
